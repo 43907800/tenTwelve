ed7c4483f0992cae5b9f74c2bfa8c9a0bce5196f
Yinn <43907800@qq.com>
2019-12-27 15:29:08 +0800

    Result 完善

--- tentwelve-common/src/main/java/com/tentwelve/model/response/QueryResponseResult.java
+++ tentwelve-common/src/main/java/com/tentwelve/model/response/QueryResponseResult.java
@@ -1,7 +1,10 @@
 package com.tentwelve.model.response;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.Data;
 import lombok.ToString;
+
+import java.util.List;
 
 /**
  * @Author: Yinn
@@ -11,13 +14,51 @@ import lombok.ToString;
 
 @Data
 @ToString
-public class QueryResponseResult extends ResponseResult {
+public class QueryResponseResult<T> extends ResponseResult {
 
-    QueryResult queryResult;
+    Object data;
 
     public QueryResponseResult(ResultCode resultCode,QueryResult queryResult){
         super(resultCode);
-        this.queryResult = queryResult;
+        this.data = queryResult;
+    }
+
+    /**
+     * 返回一个对象
+     * @param resultCode 返回 代码
+     * @param data 数据
+     */
+    public QueryResponseResult(ResultCode resultCode,Object data){
+        super(resultCode);
+        this.data = data;
+    }
+
+    /**
+     * 返回分页数据
+     * @param resultCode 返回 代码
+     * @param page 分页对象
+     */
+    public QueryResponseResult(ResultCode resultCode, Page<T> page){
+        super(resultCode);
+        QueryResult<T> queryResult = new QueryResult<>();
+        queryResult.setList(page.getRecords());
+        queryResult.setTotal(page.getTotal());
+        queryResult.setCurrent(page.getCurrent());
+        queryResult.setPages(page.getPages());
+        queryResult.setSize(page.getSize());
+        this.data = queryResult;
+    }
+
+    /**
+     * 返回list 集合数据
+     * @param resultCode 返回 代码
+     * @param list 数据
+     */
+    public QueryResponseResult(ResultCode resultCode, List<T> list){
+        super(resultCode);
+        QueryResult<T> queryResult = new QueryResult<>();
+        queryResult.setList(list);
+        this.data = queryResult;
     }
 
 }
